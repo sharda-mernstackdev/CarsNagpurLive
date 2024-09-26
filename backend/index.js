75d603a71465a9041904492713834d79078a1913
sanjanakashimkar <sanjanakashimkar1999@gmail.com>
2024-09-26 16:58:54 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -75,3 +75,12 @@ app.post('/api/verify-otp', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
+
+
+
+
+
