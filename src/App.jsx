074d4797312ca8884721023550f5238ff9729c43
sharda-mernstackdev <aws.shardawaghmare12@gmail.com>
2024-10-15 14:06:02 +0530

    modify files

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,23 @@ import Insurance from './Component/Insurance'
 import VehicleHistory from './Component/VehicleHistory'
 import TradeInVlaue from './Component/TradeInVlaue'
 import Showroom from './Component/Showroom'
-import ServiceCentre from './Component/ServiceCentre'
+
 import DealerShip from './Component/DealerShip'
-import TestDriveLocation from './Component/TestDriveLocation'
+// import TestDriveLocation from './Component/TestDriveLocation'
 import MyAppointment from './Component/MyAppointment'
 import MyBooking from './Component/MyBooking'
 import BecomePartner from './Component/BecomePartner'
 import MyOrder from './Component/MyOrder'
+import HelpCenter from './Component/HelpCenter'
+import FAQ from './Component/FAQ'
+import About from './Component/About'
+import ContactUs from './Component/ContactUs'
+import NewCars from './Component/NewCars'
+import Services from './Component/Services'
+import { Echallan } from './Component/Echallan'
+import { Policy } from './Component/Policy'
+import Testimonial from './Component/Testimonial'
+import CarUpload from './Component/CarUpload'
 
 
 function App() {
@@ -33,19 +43,26 @@ function App() {
    
     <Router >
       <Navbar/>
-      
+     
       <Routes>
         <Route path="/login" element={<Login/>} />
       </Routes>
 
       <Routes>
-        <Route path="/home" element={<Home/>} />
+        <Route path="/" element={<Home/>} />
       </Routes>
 
-      {/* <Routes>
+      <Routes>
         <Route path="/usedcars" element={<AllCars/>} />
       </Routes>
 
+      <Routes>
+        <Route path="/cardetails" element={<CarsData/>} />
+      </Routes>
+
+      <Routes>
+        <Route path="/service-centers" element={<Services/>} />
+      </Routes>
    
 
       <Routes>
@@ -70,17 +87,15 @@ function App() {
         <Route path="/showrooms" element={<Showroom/>} />
       </Routes>
 
-      <Routes>
-        <Route path="/service-centers" element={<ServiceCentre/>} />
-      </Routes>
+     
 
       <Routes>
         <Route path="/dealerships" element={<DealerShip/>} />
       </Routes>
 
-      <Routes>
+      {/* <Routes>
         <Route path="/test-drive-locations" element={<TestDriveLocation/>} />
-      </Routes>
+      </Routes> */}
 
       <Routes>
         <Route path="/my-appointments" element={<MyAppointment/>} />
@@ -91,7 +106,11 @@ function App() {
       </Routes>
 
       <Routes>
-        <Route path="/become-partner" element={<BecomePartner/>} />
+        <Route path="/my-orders" element={<MyOrder/>} />
+      </Routes>
+
+      <Routes>
+        <Route path="/carup" element={< CarUpload/>} />
       </Routes>
 
       <Routes>
@@ -99,13 +118,43 @@ function App() {
       </Routes>
 
       <Routes>
-        <Route path="/my-orders" element={<MyOrder/>} />
+        <Route path="/help" element={<HelpCenter/>} />
       </Routes>
 
-      {/* <Routes>
-        <Route path="/profile" element={<ProfileSettings/>} />
-      </Routes> */}
+      <Routes>
+        <Route path="/faq" element={<FAQ/>} />
+      </Routes>
 
+      <Routes>
+        <Route path="/about" element={<About/>} />
+      </Routes>
+
+      <Routes>
+        <Route path="/contact" element={<ContactUs/>} />
+      </Routes>
+
+      <Routes>
+        <Route path="/sell-car" element={<NewCars/>} />
+      </Routes>
+
+      <Routes>
+        <Route path="/new-car" element={<NewCars/>} />
+      </Routes>
+
+      <Routes>
+        <Route path="/challan" element={<Echallan/>} />
+      </Routes>
+
+
+      <Routes>
+        <Route path="/terms" element={<Policy/>} />
+      </Routes>
+ 
+ 
+
+      <Routes>
+        <Route path="/testimonials" element={<Testimonial/>} />
+      </Routes>
 
       <Footer/>
     </Router>
